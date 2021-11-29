42fec308a87c10ce9e353740c9f688f4d85cca05
LamGC <lam827@lamgc.net>
2021-11-29 13:02:02 +0800

    perf(database): 更改数据库连接的创建细节.
    先前 database 会比 initialDatabase 更早初始化, 导致当数据库无法连接时引发类初始化错误,
    现在通过一个 object 类, 将 database 的初始化时机推迟到首次使用 database 对象的时候,
    可使得 initialDatabase 能更早发现数据库无法连接, 进而提供更有意义的信息并退出程序.

--- src/main/kotlin/it/xiyan/automusician/Database.kt
+++ src/main/kotlin/it/xiyan/automusician/Database.kt
@@ -5,26 +5,40 @@ import org.ktorm.database.Database
 import org.ktorm.dsl.eq
 import org.ktorm.entity.find
 import org.ktorm.entity.sequenceOf
-import org.ktorm.schema.*
+import org.ktorm.schema.Table
+import org.ktorm.schema.datetime
+import org.ktorm.schema.long
+import org.ktorm.schema.varchar
 import java.sql.DriverManager
+import java.sql.SQLException
+import kotlin.system.exitProcess
 
-private val logger = KotlinLogging.logger {  }
+private val logger = KotlinLogging.logger { }
 
-val database = Database.Companion.connect(
-    url = "jdbc:mysql://${Constants.serverProp.database.address}/${Constants.serverProp.database.databaseName}",
-    user = Constants.serverProp.database.user,
-    password = Constants.serverProp.database.password,
-    generateSqlInUpperCase = true
-)
+val database
+    get() = AppDatabase.database
+
+object AppDatabase {
+    val database = Database.Companion.connect(
+        url = "jdbc:mysql://${Constants.serverProp.database.address}/${Constants.serverProp.database.databaseName}",
+        user = Constants.serverProp.database.user,
+        password = Constants.serverProp.database.password,
+        generateSqlInUpperCase = true
+    )
+}
 
 fun initialDatabase() {
-    DriverManager.getConnection(
-        "jdbc:mysql://" +
-                "${Constants.serverProp.database.address}/${Constants.serverProp.database.databaseName}?" +
-                "user=${Constants.serverProp.database.user}&password=${Constants.serverProp.database.password}"
-    ).use { connection ->
+    val jdbcUrl = "jdbc:mysql://" +
+            "${Constants.serverProp.database.address}/${Constants.serverProp.database.databaseName}?" +
+            "user=${Constants.serverProp.database.user}&password=${Constants.serverProp.database.password}"
+    if (!checkDatabaseConnectivity(jdbcUrl)) {
+        exitProcess(1)
+    }
+
+    DriverManager.getConnection(jdbcUrl).use { connection ->
         val statement = connection.createStatement()
-        val result = statement.executeUpdate("""
+        val result = statement.executeUpdate(
+            """
             CREATE TABLE IF NOT EXISTS ${NeteaseCloudUserPO.tableName} (
                 uid BIGINT PRIMARY KEY NOT NULL,
                 cookies TEXT NOT NULL,
@@ -41,7 +55,19 @@ fun initialDatabase() {
     }
 }
 
-object NeteaseCloudUserPO: Table<NeteaseCloudUser>("musician_users") {
+fun checkDatabaseConnectivity(jdbcUrl: String): Boolean {
+    return try {
+        DriverManager.getConnection(jdbcUrl).use {
+            it.close()
+            true
+        }
+    } catch (e: SQLException) {
+        logger.error { "Unable to connect to database: [${e.errorCode}] ${e.message}" }
+        false
+    }
+}
+
+object NeteaseCloudUserPO : Table<NeteaseCloudUser>("musician_users") {
     // id 是 音乐工人 里面的唯一 id, 可以记录其他信息.
     private val id = long("uid").primaryKey().bindTo { it.uid }
     val cookies = varchar("cookies").bindTo { it.cookies }
